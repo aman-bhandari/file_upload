a1c7466b9d31f7579af79fcfee0a4c00080ff657
Aman <bhandari.aman0101@gmail.com>
2022-09-24 23:14:15 +0530

    image upload validation added

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,12 +1,21 @@
 const { StatusCodes } = require('http-status-codes')
 const path = require('path')
+const customError = require('../errors')
 const uploadProductImage = async (req, res) => {
   // check if file exists
+  if (!req.files) throw new customError.BadRequestError('No file uploaded')
+  const productImage = req.files.image
 
   //check format
+  if (!productImage.mimetype.startsWith('image'))
+    throw new customError.BadRequestError('Please upload an image')
   // check size
+  const maxSize = 1024 * 1024 * 5
+  if (productImage.size > maxSize)
+    throw new customError.BadRequestError(
+      `Please upload image samller than ${maxSize} bytes`
+    )
 
-  const productImage = req.files.image
   const imagePath = path.join(
     __dirname,
     '../public/uploads',
