ed571b47bf26dece7e024be355ec24f43f90e341
Aman <bhandari.aman0101@gmail.com>
2022-09-25 12:43:27 +0530

    setup completed

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,33 +1,43 @@
-const { StatusCodes } = require('http-status-codes')
 const path = require('path')
-const customError = require('../errors')
-const uploadProductImage = async (req, res) => {
-  // check if file exists
-  if (!req.files) throw new customError.BadRequestError('No file uploaded')
+const { StatusCodes } = require('http-status-codes')
+const CustomError = require('../errors')
+const cloudinary = require('cloudinary').v2
+const fs = require('fs')
+
+const uploadProductImageLocal = async (req, res) => {
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No File Uploaded')
+  }
   const productImage = req.files.image
-
-  //check format
-  if (!productImage.mimetype.startsWith('image'))
-    throw new customError.BadRequestError('Please upload an image')
-  // check size
-  const maxSize = 1024 * 1024 * 5
-  if (productImage.size > maxSize)
-    throw new customError.BadRequestError(
-      `Please upload image samller than ${maxSize} bytes`
-    )
-
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please Upload Image')
+  }
+  const maxSize = 1024 * 1024
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError('Please upload image smaller 1MB')
+  }
   const imagePath = path.join(
     __dirname,
-    '../public/uploads',
-    `${productImage.name}`
+    '../public/uploads/' + `${productImage.name}`
   )
   await productImage.mv(imagePath)
-  res.status(StatusCodes.OK).json({
-    image: {
-      src: `uploads/${productImage.name}`,
-    },
-  })
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } })
 }
+
+const uploadProductImage = async (req, res) => {
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    {
+      use_filename: true,
+      folder: 'file-upload',
+    }
+  )
+  fs.unlinkSync(req.files.image.tempFilePath)
+  return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } })
+}
+
 module.exports = {
   uploadProductImage,
 }
